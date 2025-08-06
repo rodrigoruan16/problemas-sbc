a86eb35bf99b7cc83fc3304c5473b8751166c9bc
rodrigoruan16 <rodrigoruan16@hotmail.com>
2025-08-05 22:18:47 -0300

    Adiciona cachorros quentes

---
+++ 2016/CachorrosQuentes.cpp
@@ -0,0 +1,14 @@
+// Autor: Rodrigo Ruan - Projeto Maratona de Programacao - UNIFEI
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    double h, p;
+    cin >> h >> p;
+
+    cout << fixed << setprecision(2) << h/p << "\n";
+
+    return 0;
+}
